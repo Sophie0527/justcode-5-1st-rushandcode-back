2859ac6614a060a285b7cdb4562c2dc2e1585be9
happyeveryone96 <happyeveryone@naver.com>
2022-06-21 15:02:38 +0900

    Add: 백엔드 초기 설정

---
+++ database/data/04.review_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO reviews(id, product_id, user_id, name, content)
+VALUES
+(1, 1, 1, "codeKim","리뷰 1"),
+(2, 1, 2, "codeLee","리뷰 2"),
+(3, 2, 3, "codePark", "리뷰 3"),
+(4, 3, 3, "codePark", "리뷰 4");
