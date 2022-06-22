e40aeb203a480c0f557ad0dbf1fab6aec705ff9f
happyeveryone96 <happyeveryone@naver.com>
2022-06-22 12:48:15 +0900

    Refactor: 중복되는 코드 제거

--- models/product.js
+++ models/product.js
@@ -1,139 +1,61 @@
 const prisma = require('./prisma-client');
 
 async function readProducts(mainCategory, subCategory, sort) {
+  const productsQuery = `SELECT products.name, products.hashtags, products.price,
+                         JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
+                         FROM products
+                         JOIN (
+                           SELECT * FROM images) as IMG
+                         ON IMG.product_id = products.id
+                         `;
+  const group = ' GROUP BY products.id';
+  const desc = ' ORDER BY price desc';
+  const asc = ' ORDER BY price asc';
+  const sellCountDesc = ' ORDER BY sell_count desc';
+  const main = ` WHERE main_category="${mainCategory}"`;
+  const sub = ` AND sub_category="${subCategory}"`;
+
   if (mainCategory === undefined) {
     if (sort === 'desc') {
       // 높은가격순
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        GROUP BY products.id
-        ORDER BY price desc;
-      `;
+      return await prisma.$queryRawUnsafe(productsQuery + group + desc);
     } else if (sort === 'asc') {
       // 낮은가격순
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        GROUP BY products.id
-        ORDER BY price asc;
-      `;
+      return await prisma.$queryRawUnsafe(productsQuery + group + asc);
     } else if (sort === 'sell') {
       // 판매인기순
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        GROUP BY products.id
-        ORDER BY sell_count desc;
-      `;
+      return await prisma.$queryRawUnsafe(
+        productsQuery + group + sellCountDesc
+      );
     }
     // 추천순
-    return await prisma.$queryRaw`
-      SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-      FROM products
-      JOIN (
-        SELECT * FROM images) as IMG
-      ON IMG.product_id = products.id
-      GROUP BY products.id
-      ORDER BY price desc;
-    `;
+    return await prisma.$queryRawUnsafe(productsQuery + group);
   } else if (mainCategory !== undefined && subCategory === undefined) {
     if (sort === 'desc') {
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        WHERE main_category=${mainCategory}
-        GROUP BY products.id
-        order by price desc;
-      `;
+      return await prisma.$queryRawUnsafe(productsQuery + main + group + desc);
     } else if (sort === 'asc') {
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        WHERE main_category=${mainCategory}
-        GROUP BY products.id
-        order by price asc;
-      `;
+      return await prisma.$queryRawUnsafe(productsQuery + main + group + asc);
     } else if (sort === 'sell') {
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        WHERE main_category=${mainCategory}
-        GROUP BY products.id
-        order by sell_count desc;
-      `;
+      return await prisma.$queryRawUnsafe(
+        productsQuery + main + group + sellCountDesc
+      );
     }
-    return await prisma.$queryRaw`
-      SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-      FROM products
-      JOIN (
-        SELECT * FROM images) as IMG
-      ON IMG.product_id = products.id
-      WHERE main_category=${mainCategory}
-      GROUP BY products.id;
-    `;
+    return await prisma.$queryRawUnsafe(productsQuery + main + group);
   } else if (mainCategory !== undefined && subCategory !== undefined) {
     if (sort === 'desc') {
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        WHERE main_category=${mainCategory} and sub_category=${subCategory}
-        GROUP BY products.id
-        order by price desc;
-      `;
+      return await prisma.$queryRawUnsafe(
+        productsQuery + main + sub + group + desc
+      );
     } else if (sort === 'asc') {
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        WHERE main_category=${mainCategory} and sub_category=${subCategory}
-        GROUP BY products.id
-        order by price asc;
-      `;
+      return await prisma.$queryRawUnsafe(
+        productsQuery + main + sub + group + asc
+      );
     } else if (sort === 'sell') {
-      return await prisma.$queryRaw`
-        SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-        FROM products
-        JOIN (
-          SELECT * FROM images) as IMG
-        ON IMG.product_id = products.id
-        WHERE main_category=${mainCategory} and sub_category=${subCategory}
-        GROUP BY products.id
-        order by sell_count desc;
-      `;
+      return await prisma.$queryRawUnsafe(
+        productsQuery + main + sub + group + sell
+      );
     }
-    return await prisma.$queryRaw`
-      SELECT products.name, products.hashtags, products.price, JSON_ARRAYAGG(JSON_OBJECT('id', IMG.id, 'url', IMG.image_url)) productImages
-      FROM products
-      JOIN (
-        SELECT * FROM images) as IMG
-      ON IMG.product_id = products.id
-      WHERE main_category=${mainCategory} and sub_category=${subCategory}
-      GROUP BY products.id;
-    `;
+    return await prisma.$queryRawUnsafe(productsQuery + main + sub + group);
   }
 }
 
