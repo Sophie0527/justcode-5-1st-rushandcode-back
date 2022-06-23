47dfe6f1aaff2a948a7849c4ad772e15e257a968
happyeveryone96 <happyeveryone@naver.com>
2022-06-23 14:46:06 +0900

    Fix: 유저 스키마, 리뷰 스키마 수정

--- database/schema/04.create_reviews.sql
+++ database/schema/04.create_reviews.sql
@@ -5,6 +5,7 @@ CREATE TABLE reviews
   product_id INT NOT NULL,
   name VARCHAR(30) NOT NULL,
   content TEXT NOT NULL,
+  stars INT NOT NULL,
   creataed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
