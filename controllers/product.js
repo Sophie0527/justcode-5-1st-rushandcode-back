dc80b6b714235ae949367a8ec78750099688621d
happyeveryone96 <happyeveryone@naver.com>
2022-06-24 18:59:54 +0900

    Refactor: product 서비스 내부 함수명 수정

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,13 @@
-const { getAllProducts } = require('../services/product');
+const { getProductsByCondition } = require('../services/product');
 
 const getProducts = async (req, res) => {
   try {
     const { mainCategory, subCategory, sort } = req.query;
-    const products = await getAllProducts(mainCategory, subCategory, sort);
+    const products = await getProductsByCondition(
+      mainCategory,
+      subCategory,
+      sort
+    );
     return res.status(200).json({ message: 'OK', products });
   } catch (err) {
     console.log(err);
--- services/product.js
+++ services/product.js
@@ -1,7 +1,7 @@
 const { readProducts } = require('../models/product');
 
-async function getAllProducts(mainCategory, subCategory, sort) {
+async function getProductsByCondition(mainCategory, subCategory, sort) {
   return await readProducts(mainCategory, subCategory, sort);
 }
 
-module.exports = { getAllProducts };
+module.exports = { getProductsByCondition };
