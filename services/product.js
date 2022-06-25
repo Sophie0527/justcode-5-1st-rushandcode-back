edeb2e21be9810ebf0b7a5b41370700eac15f7bf
손소희 <sonsohee@sonsohuis-MacBook-Air.local>
2022-06-25 15:44:05 +0900

    Fix: 이미지 댓글 중복 및  일부 데이터 호출 불가 수정

--- services/product.js
+++ services/product.js
@@ -5,7 +5,32 @@ async function getProductsByCondition(mainCategory, subCategory, sort) {
 }
 
 async function getProductDetail(id) {
-  return await readProductDetail(id);
+  const getProductDetail = await readProductDetail(id);
+
+  const filter = (arr, key) => {
+    for (let i = 0; i < getProductDetail.length; i++) {
+      let filteredItem = arr[i].filter((item, idx) => {
+        return (
+          arr[i].findIndex(item2 => {
+            return item.id === item2.id;
+          }) === idx
+        );
+      });
+      getProductDetail[i][key] = filteredItem;
+    }
+  };
+
+  const images = getProductDetail.map(e => {
+    return e.productImages;
+  });
+  filter(images, 'productImages');
+
+  const reviews = getProductDetail.map(e => {
+    return e.productReviews;
+  });
+  filter(reviews, 'productReviews');
+
+  return getProductDetail;
 }
 
 module.exports = { getProductsByCondition, getProductDetail };
