9905745756512242ac9ec25e9fd2e769f8311705
happyeveryone96 <happyeveryone@naver.com>
2022-06-30 13:22:37 +0900

    Fix: product 모델 수정

--- models/product.js
+++ models/product.js
@@ -14,7 +14,7 @@ async function readProducts(mainCategory, subCategory, sort) {
   const sellCountDesc = ' ORDER BY sell_count desc';
   const main = ` WHERE main_category="${mainCategory}"`;
   const sub = ` AND sub_category="${subCategory}"`;
-  const banner = ` Limit 12`; 
+  const banner = ` Limit 12`;
 
   if (mainCategory === undefined) {
     if (sort === 'desc') {
@@ -58,7 +58,7 @@ async function readProducts(mainCategory, subCategory, sort) {
       );
     } else if (sort === 'sell') {
       return await prisma.$queryRawUnsafe(
-        productsQuery + main + sub + group + sell
+        productsQuery + main + sub + group + sellCountDesc
       );
     }
     return await prisma.$queryRawUnsafe(productsQuery + main + sub + group);
